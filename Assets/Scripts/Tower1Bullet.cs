54cb02138529378e33a1dc1203fc4ac94e31bf0c
LeonWubben <leon_wubben1@live.nl>
2014-11-23 16:30:25 +0100

    Proberen github te fixen..

---
+++ Assets/Scripts/Tower1Bullet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tower1Bullet : MonoBehaviour {
+
+	private GameObject target; //the gameobject the bullet is going towards
+	private Vector2 position; // the startposition of the object
+	private GameObject bullet;  // the prefab of the bullet
+	public float speed;
+	void Awake(){
+		//target = Tower1.targetObject;
+		}
+
+	//todo, damage the target
+	void OnTriggerEnter(Collider col){
+				if (col.gameObject.name.Equals (target.gameObject.name))
+						Destroy (gameObject);
+		}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float step = speed * Time.deltaTime;
+		gameObject.transform.position = Vector3.MoveTowards (transform.position, target.transform.position,step);
+
+	}
+}
