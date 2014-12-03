73de2df364287d6dc457348012e98f4f539c1ac6
Tiamur <ktiamur@gmail.com>
2014-12-03 15:17:44 +0100

    Script dat een wave spawnt

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public int numOfEnemies;
+    List<GameObject> enemies;
+    public float SpawnRate = 1f;
+    private float spawnRange = 10;
+    public float maxX;
+    public float maxZ;
+    float orcHeigthSpawn = 3.27f;
+    public bool spawning = true;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //InvokeRepeating("Spawning", 0, 3);
+        GameObject Enemy = (GameObject)Instantiate(enemy, transform.position + new Vector3(0f, orcHeigthSpawn, 0f), Quaternion.identity);
+        Enemy.name = "enemy";
+        Enemy.transform.FindChild("Floor").transform.position = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float rand=Random.value;
+        if (rand < 1/SpawnRate && spawning)
+        {
+            float randX = Random.Range(-maxX / 2, maxX / 2);
+            float randZ = Random.Range(-maxZ / 2, maxZ / 2);
+
+            GameObject Enemy = (GameObject)Instantiate(enemy, transform.position + new Vector3(randX, 0f, randZ), Quaternion.identity);
+            Enemy.name = "enemy";   
+            enemies.Add(Enemy);
+        }
+
+        if (enemies.Count < numOfEnemies) {
+            spawning = false;
+        }
+    }
+
+    void Spawning()
+    {
+        if (SpawnRate > 5)
+            SpawnRate = SpawnRate - 1;
+    }
+
+}
+
