899ff1448a9fb90367076238c5680202a167ae26
Niels Versluis <N.J.Versluis@student.tudelft.nl>
2014-12-04 12:24:03 +0100

    Made major improvements to Navigator. Sadly it causes infinite loops, so i'll be working on that.

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -14,7 +14,7 @@ public class EnemyScript : MonoBehaviour {
 
         characterController = GetComponent<CharacterController>();
 
-        Path = Navigator.Path(transform.FindChild("Floor").transform.position, PlayerController.location - new Vector3(0f, PlayerController.location.y, 0f));
+        //Path = Navigator.Path(transform.FindChild("Floor").transform.position, PlayerController.location - new Vector3(0f, PlayerController.location.y, 0f));
 
 	}
 	
@@ -36,7 +36,7 @@ public class EnemyScript : MonoBehaviour {
         {
             i = 0;
         }
-        if (Input.GetKeyDown(KeyCode.Space) || Path == null)
+        if (Input.GetKeyDown(KeyCode.Space)/* || Path == null*/)
         {
             Debug.Log("Space Ingedrukt");
             Path = Navigator.Path(transform.FindChild("Floor").transform.position, PlayerController.location - new Vector3(0f, PlayerController.location.y, 0f));
