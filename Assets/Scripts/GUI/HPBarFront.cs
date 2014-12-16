1662abf82b20a70914fb4f22dc677b9ab6ae03ec
Niels Versluis <N.J.Versluis@student.tudelft.nl>
2014-12-16 11:34:42 +0100

    Fixed main menu button sizes, remade base GUI.

--- Assets/Scripts/GUI/HPBarFront.cs
+++ Assets/Scripts/GUI/HPBarFront.cs
@@ -45,7 +45,7 @@ public class HPBarFront : MonoBehaviour {
 		maxHP = 100f;
         /* DEBUG */
 		if (bufferedHP < currentHP) {
-			if (System.Math.Abs(bufferedHP - currentHP) < (maxHP/100f)) {
+			if (System.Math.Abs(bufferedHP - currentHP) < (maxHP/1000f)) {
 				bufferedHP = currentHP;
 			}
 			else{
