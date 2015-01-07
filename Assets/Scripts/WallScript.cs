9f21df117f4a9fe4bda8b09e64a20662385f943f
LeonWubben <leon_wubben1@live.nl>
2015-01-07 20:58:37 +0100

    Towers sellen nu door eerst 7 te selecteren en vervolgen rechtermuis

--- Assets/Scripts/WallScript.cs
+++ Assets/Scripts/WallScript.cs
@@ -4,10 +4,12 @@ using System.Collections;
 
 //Script to attach to the walls. Will instantiate hotspots on walls if player is building towers.
 
-public class WallScript : MonoBehaviour {
+public class WallScript : MonoBehaviour
+{
 			
-	Color transparentgreen = new Color(0,255,0,0.1f); //Color of the green hotspot
-	Color transparentred = new Color(255,0,0,0.1f); 
+	Color transparentgreen = new Color (0, 255, 0, 0.1f);
+	//Color of the green hotspot
+	Color transparentred = new Color (255, 0, 0, 0.1f);
 	Color curColor;
 	string curTag;
 	private GameObject ResourceManagerObj;
@@ -15,45 +17,49 @@ public class WallScript : MonoBehaviour {
 	float planeW;
 	private GameObject player;
 	private float maxDistance;
-	private int  cost;
+	private int cost;
 	private PlayerData playerData;
+	private static GameObject resTower;
+	private KeyInputManager inputManager;
 
-	void Start(){
+	void Start ()
+	{
 		ResourceManagerObj = GameObject.Find ("ResourceManager");
 		resourceManager = ResourceManagerObj.GetComponent<ResourceManager> ();
 		planeW = resourceManager.planewidth;
 		maxDistance = resourceManager.maxTowerDistance;
 		playerData = GUIScript.player;
 		cost = 0;
+		inputManager = GameObject.Find ("KeyInputs").GetComponent<KeyInputManager> ();
 	}
 
-	void Update(){
+	void Update ()
+	{
 
 		if (gameObject == CameraController.hitObject) { //if the object you are looking at is the wall
 			Vector3 normal = CameraController.hit.normal;
-			DestroyHotSpots (); //Destroy all objects
-		
+			//Destroy all objects
+			if (gameObject.transform.childCount != 2) {
+				DestroyHotSpots ();
+			}
 			GameObject TowerPrefab = WeaponController.curTower;
-			if (TowerPrefab != null &&gameObject.transform.childCount==0) { 
+			if (TowerPrefab != null && gameObject.transform.childCount == 0) { 
 				Vector3 normalToWall = CameraController.hit.normal;
-				Vector3 TowerOffset = new Vector3 (Mathf.Sin (transform.eulerAngles.y / 180 * Mathf.PI), 0, Mathf.Cos (transform.eulerAngles.y / 180 * Mathf.PI)) * planeW/50;
-				GameObject tower = (GameObject)Instantiate (TowerPrefab, new Vector3(transform.position.x,planeW/2,transform.position.z), transform.rotation);
-				tower.gameObject.transform.localScale = new Vector3 (1, 1, 1) * planeW*10;
-				tower.gameObject.transform.Rotate(new Vector3 (-90, 0, 0));
-				tower.gameObject.transform.Rotate(new Vector3 (0, -90, 0));
-				tower.gameObject.transform.position += tower.gameObject.transform.forward*planeW/58;
-				if (player== null) {
+				Vector3 TowerOffset = new Vector3 (Mathf.Sin (transform.eulerAngles.y / 180 * Mathf.PI), 0, Mathf.Cos (transform.eulerAngles.y / 180 * Mathf.PI)) * planeW / 50;
+				GameObject tower = (GameObject)Instantiate (TowerPrefab, new Vector3 (transform.position.x, planeW / 2, transform.position.z), transform.rotation);
+				tower.gameObject.transform.localScale = new Vector3 (1, 1, 1) * planeW * 10;
+				tower.gameObject.transform.Rotate (new Vector3 (-90, 0, 0));
+				tower.gameObject.transform.Rotate (new Vector3 (0, -90, 0));
+				tower.gameObject.transform.position += tower.gameObject.transform.forward * planeW / 58;
+				if (player == null) {
 					player = GameObject.Find ("Player");
 				}
-
-
 				//set costs
-				if(TowerPrefab.name.Contains("magic")){
+				if (TowerPrefab.name.Contains ("magic")) {
 					cost = resourceManager.costMagicTower;
 				}
 
-
-				if ((Vector3.Distance (player.transform.position, transform.position) <= maxDistance ) && cost <= playerData.getGold()) {
+				if ((Vector3.Distance (player.transform.position, transform.position) <= maxDistance) && cost <= playerData.getGold ()) {
 					curColor = transparentgreen;
 					curTag = "TowerHotSpot";
 				} else {
@@ -61,87 +67,87 @@ public class WallScript : MonoBehaviour {
 					curTag = "HotSpotRed";
 				}
 
-
 				tower.tag = curTag;
-
-				foreach (Renderer child in tower.GetComponentsInChildren<Renderer>()) {
-
+				foreach (Renderer child in tower.GetComponentsInChildren<Renderer>()) 
+				{
 					child.material.color = curColor;
 					child.material.shader = Shader.Find ("Transparent/Diffuse");
-				}
-
-//				
-                tower.layer = 14;
+				}				
+				tower.layer = 14;
 				tower.transform.parent = gameObject.transform;
-
 			}
 			//Sell the tower
-			if (gameObject.transform.childCount == 1) {
-				if (Input.GetKeyUp (KeyCode.I)) {
-					sellTower ();
+			if (WeaponController.weapon == 50) {
+				if (gameObject.transform.childCount == 1) {
+					resTower = (GameObject)Instantiate (gameObject.transform.GetChild (0).gameObject, new Vector3 (transform.position.x, planeW / 2, transform.position.z), transform.rotation);
+					resTower.gameObject.transform.Rotate (new Vector3 (-90, 0, 0));
+					resTower.gameObject.transform.Rotate (new Vector3 (0, -90, 0));
+					resTower.gameObject.transform.localScale = new Vector3 (1, 1, 1) * 1.1f;
+					resTower.name = "blueTower";
+					resTower.gameObject.transform.parent = gameObject.transform;
+					resTower.tag = "TowerHotSpot";
+					resTower.transform.GetChild (0).gameObject.SetActive (false);
+					foreach (Renderer child in resTower.GetComponentsInChildren<Renderer>()) 
+					{
+						child.material.color = new Color (0, 0, 255, 0.1f);
+						child.material.shader = Shader.Find ("Transparent/Diffuse");
+					}
 				}
-
-				//upgrade the tower - todo
-				if (Input.GetKeyUp (KeyCode.U)) {
+				if (Input.GetKeyUp (inputManager.upgradeInput)) {
 					upgradeTower ();
 				}
 
+				//upgrade the tower - todo
+				if (Input.GetKeyUp (inputManager.sellInput)) {
+					sellTower ();
+				}
+
 				//bring up the menu to show ot sell or upgrade tower (with costs) and upgrade stats
-				if (Input.GetMouseButtonDown(1)) {
-					showMenu ();
+				if (Input.GetMouseButtonDown (1)) {
+					//	showMenu ();
 				}
 
 				if (Input.GetKeyUp (KeyCode.C)) {
-					GameObject.Find ("GUIMain").GetComponent<GUIScript> ().getPopUpPanel().SetActive (false);
+					GameObject.Find ("GUIMain").GetComponent<GUIScript> ().getPopUpPanel ().SetActive (false);
 				}
 			}
+
 		}
 
 	}
 
-	private void sellTower(){
-
+	private void sellTower ()
+	{
+		DestroyHotSpots ();
 		Destroy (gameObject.transform.GetChild (0).gameObject);
 		GUIScript.player.addGold (cost / 2);
-		GameObject.Find ("GUIMain").GetComponent<GUIScript> ().getPopUpPanel().SetActive (false);
+		GameObject.Find ("GUIMain").GetComponent<GUIScript> ().getPopUpPanel ().SetActive (false);
 	}
 
-	private void upgradeTower(){
+	private void upgradeTower ()
+	{
 		Debug.Log ("Not yet implemented");
-		GameObject.Find ("GUIMain").GetComponent<GUIScript> ().getPopUpPanel().SetActive (false);
+		GameObject.Find ("GUIMain").GetComponent<GUIScript> ().getPopUpPanel ().SetActive (false);
 	}
 
-	private void showMenu(){
-		GameObject popUpPanel = GameObject.Find ("GUIMain").GetComponent<GUIScript> ().getPopUpPanel();
+	private void showMenu ()
+	{
+		GameObject popUpPanel = GameObject.Find ("GUIMain").GetComponent<GUIScript> ().getPopUpPanel ();
 		popUpPanel.SetActive (true);
-//		Button sellTowerBut = popUpPanel.transform.GetChild (0).gameObject.GetComponent<Button>();
-//		sellTowerBut.onClick.RemoveAllListeners ();
-//		sellTowerBut.onClick.AddListener (delegate {
-//			sellTower ();
-//		});
-//		Button upgradeTowerBut = popUpPanel.transform.GetChild (1).gameObject.GetComponent<Button>();
-//		upgradeTowerBut.onClick.RemoveAllListeners ();
-//		upgradeTowerBut.onClick.AddListener (delegate {
-//			upgradeTower ();
-//		});
-//		Button cancelTowerBut = popUpPanel.transform.GetChild (2).gameObject.GetComponent<Button>();
-//		cancelTowerBut.onClick.RemoveAllListeners ();
-//		cancelTowerBut.onClick.AddListener (delegate {
-//			popUpPanel.SetActive (false);
-//		});
 	}
 
 
 	//Method to Destroy all hotspots. Called in different scripts.
-	public static void DestroyHotSpots(){
+	public static void DestroyHotSpots ()
+	{
 		GameObject[] GreenHotspot = GameObject.FindGameObjectsWithTag ("TowerHotSpot");
 		GameObject[] RedHotspot = GameObject.FindGameObjectsWithTag ("HotSpotRed");
 		foreach (GameObject hotspot in GreenHotspot)
-						Destroy (hotspot);
+			Destroy (hotspot);
 		foreach (GameObject hotspot in RedHotspot)
-						Destroy (hotspot);
+			Destroy (hotspot);
 
-		}
+	}
 
 
 }
