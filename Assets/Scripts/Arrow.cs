25990286d4b055aa2b40191b61ae41cbbd932729
LeonWubben <leon_wubben1@live.nl>
2015-01-08 19:44:19 +0100

    Arrowtower werkt en is totaal niet overpowered ofzo..

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrow : MonoBehaviour {
+	public int damagePerShot;// = 1500;
+	//Transform Player;
+	Vector3 PrevItLoc;
+	private float maxBulletDistance = 200;
+	public GameObject Boom;
+	LayerMask ignoreMask = ~(1 << 13);
+	bool headshot;
+
+	void GotThrough()
+	{
+		RaycastHit hit;
+		if (Physics.Raycast(PrevItLoc, transform.position - PrevItLoc, out hit, (PrevItLoc - transform.position).magnitude + 0.2f, ignoreMask))
+		{
+			EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
+			Destroy(this.gameObject);
+			GameObject boom = (GameObject)Instantiate(Boom, PrevItLoc, Quaternion.identity);
+			if (hit.rigidbody != null)
+			{
+				boom.rigidbody.velocity = hit.collider.rigidbody.velocity;
+				boom.GetComponent<BoomParticleScript>().Hit = hit.collider.gameObject;
+			}
+			if (enemyHealth != null)
+			{
+				enemyHealth.TakeDamage(damagePerShot, "magic");
+			}
+
+		}
+		PrevItLoc = transform.position;
+	}
+
+
+	// Use this for initialization
+	void Start()
+	{
+		//Player = GameObject.Find("Player").transform;
+		PrevItLoc = transform.position;
+	}
+
+	void FixedUpdate()
+	{
+		GotThrough();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		if ((PrevItLoc- transform.position).magnitude >maxBulletDistance)
+		{
+			Destroy(this.gameObject);
+		}
+
+	}
+}
--- Assets/Scripts/ArrowTower.cs
+++ Assets/Scripts/ArrowTower.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ArrowTower : MonoBehaviour {
 
@@ -7,7 +8,8 @@ public class ArrowTower : MonoBehaviour {
 	private ResourceManager resourceManager;
 	private string enemyTag;
 	private GameObject arrow;
-	private bool isShooting;
+	private List<GameObject> enemyOnTrap;
+	public int amountOfArrows;
 
 	// Use this for initialization
 	void Start () {
@@ -15,26 +17,61 @@ public class ArrowTower : MonoBehaviour {
 		resourceManager = ResourceManagerObj.GetComponent<ResourceManager> ();
 		enemyTag = resourceManager.enemyTag;
 		arrow = resourceManager.arrowTowerArrow;
-		isShooting = false;
+		enemyOnTrap=new List<GameObject>();
+		amountOfArrows = 20;
 	}
 
 	void OnTriggerEnter(Collider col)
 	{
-		if (col.gameObject.tag == enemyTag && !isShooting) {
-			InvokeRepeating ("Shootbullet", 0, 0.5f);
-			isShooting = true;
+		if (col.gameObject.tag == enemyTag &&!enemyOnTrap.Contains(col.gameObject)) {
+			enemyOnTrap.Add (col.gameObject);
+			if (enemyOnTrap.Count == 1) {
+				InvokeRepeating ("Shooting", 0, 2/transform.parent.gameObject.GetComponent<TowerStats>().speed);
+			}
+		}
+	}
 
+	void OnTriggerExit(Collider col)
+	{
+		if(col.gameObject.tag == enemyTag)
+		{
+			enemyOnTrap.Remove(col.gameObject);
+		}
+	}
+
+	private void Shooting()
+	{
+		for (int i = 0; i < amountOfArrows; i++) {
+			Invoke ("Shootbullet", i*0.02f);
 		}
 	}
 
 	private void Shootbullet()
 	{
+		//Vector3 positionOffset = new Vector3(
 		GameObject bullet = (GameObject)Instantiate (arrow, transform.parent.position, Quaternion.identity);
+		bullet.transform.parent = gameObject.transform.parent;
+		bullet.transform.localPosition = new Vector3 (0, 3.8f - Mathf.Floor (Random.value * 8) * 1.075f, 3.775f - Mathf.Floor (Random.value * 8) * 1.075f);
 		bullet.transform.up = -transform.right;
+		bullet.GetComponent<Arrow> ().damagePerShot = transform.parent.gameObject.GetComponent<TowerStats> ().attack;
 		bullet.rigidbody.velocity = -transform.right*20;
 	}
+
+
+
+
 	// Update is called once per frame
 	void Update () {
-	
+		enemyOnTrap.RemoveAll (item => item == null);
+		for (int i = 0; i < enemyOnTrap.Count; i++) {
+			EnemyResources enemyResources = enemyOnTrap[i].collider.GetComponent<EnemyResources>();
+			if (enemyResources.isDead)
+			{
+				enemyOnTrap.Remove(enemyOnTrap[i]);
+			}
+		}
+		if (enemyOnTrap.Count == 0) {
+			CancelInvoke ();
+		} 
 	}
 }
