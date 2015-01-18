fd463b5446b5ffddab58da7912ec4cf0b2b087fc
borismul <boris.mulder@live.nl>
2015-01-18 15:04:52 +0100

    begin en eind icoontjes toegevoegd kan nog een ander icoontje worden als jullie het niet goed vinden

---
+++ Assets/Scripts/iconsScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class iconsScript : MonoBehaviour {
+
+    GameObject player;
+	
+	// Update is called once per frame
+	void Update () {
+        //change the layer of the minimapplane of this floor if the player is too far, so it wont show on the minimap
+        if (player == null)
+        {
+            player = GameObject.Find("Player");
+        }
+        else if ((player.transform.position - transform.position).magnitude >= 70)
+        {
+            gameObject.layer = 0;
+        }
+        else
+        {
+            gameObject.layer = 9;
+        }
+	}
+}
