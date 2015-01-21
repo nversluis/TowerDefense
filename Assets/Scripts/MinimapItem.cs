708a240583ab3a3e662b459ae75a925c3a1e14be
LeonWubben <leon_wubben1@live.nl>
2015-01-21 15:49:05 +0100

    no message

---
+++ Assets/Scripts/MinimapItem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinimapItem : MonoBehaviour {
+
+	GameObject player;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player == null) {
+			player = GameObject.Find ("Player");
+		}
+		else if ((player.transform.position - transform.position).magnitude >= 70) {
+			transform.GetChild(0).gameObject.layer = 16;
+		} else {
+			transform.GetChild(0).gameObject.layer = 9;
+		}
+	
+	}
+}
