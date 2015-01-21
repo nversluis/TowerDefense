574a3bf2d0fb856d149d84af6be9ecd82e3af928
Niels Versluis <N.J.Versluis@student.tudelft.nl>
2015-01-21 19:44:29 +0100

    Made Login screen bigger

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -294,7 +294,7 @@ public class MenuController : MonoBehaviour {
                 scorePanelList[10].SetActive(true);
                 rankList[10].text = ScoreServer.getPositionOnHiscores((int)scoreDifficulty.value, PlayerPrefs.GetString("Login")).ToString() + ":";
                 nameList[10].text = PlayerPrefs.GetString("Login");
-                hiScoreList[10].text = HiScores[ScoreServer.getPositionOnHiscores((int)scoreDifficulty.value, PlayerPrefs.GetString("Login"))-1][1];
+                hiScoreList[10].text = HiScores[ScoreServer.getPositionOnHiscores((int)scoreDifficulty.value, PlayerPrefs.GetString("Login"))][1];
             }
             else {
                 scorePanelList[10].SetActive(false);
